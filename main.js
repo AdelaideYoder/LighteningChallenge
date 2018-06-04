b1242a7a23524e8300381ee9b63f7b2ea937af2f
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-06-04 09:39:59 -0500

    added Jisie's code

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// Lightening Exercise 1 - Week 2-----------------------------------------------------------
+// let experience = ["fun", "challenging", "interesting", "new", "different"];
+
+
+// for (let i = 0; i < experience.length; i++ ) {
+//     console.log (experience[i]);
+// }
+
+
+//Lightening Exercise 2 - Week 3 - Monday June 4, 2018____________________________________________________
 // Exercise: Using Javascript, create a variable to hold your last name. Then, if your last name has 7 or more letters, console log "I can't pronounce this last name". If your last name has less than 7 characters, console log your last name.
 
 
@@ -14,17 +24,14 @@ let name = {} => {
 }
 } 
 
-if (lastName.length >= 7) {
-    console.log ("I can't pronounce this last name");
-} else {
-    console.log("Last name:", lastName); 
-}
+// This is the way Jisie did it______________________________________________
+// const lastName = "Yoder";
 
-
-
-
-
-
+// if (lastName.length >= 7) {
+//     console.log ("I can't pronounce this last name");
+// } else {
+//     console.log("Last name:", lastName); 
+// }
 
 
 
