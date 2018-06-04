837809893c0661a12092c24cc4679c853d01d595
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-06-04 09:14:06 -0500

    added lightening challenge

---
+++ main.js
@@ -0,0 +1,27 @@
+// Exercise: Using Javascript, create a variable to hold your last name. Then, if your last name has 7 or more letters, console log "I can't pronounce this last name". If your last name has less than 7 characters, console log your last name.
+
+
+// create a variable to hold your last name
+
+const lastName = "Yoder";
+
+let name = {} => {
+    if (lastName >= 7) {
+    console.log("I can't pronounce this last name");
+    }
+    else if (lastName < 7) {
+    console.log(lastName);
+}
+} 
+
+
+
+
+
+
+
+
+
+
+
+// Challenge: Create a new repository on Github and push the code your wrote up to Github.
